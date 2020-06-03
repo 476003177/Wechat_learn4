96d50b53e6400858411a119747854dc956147a9a
unknown <unknown@example.com>
2020-06-03 09:31:40 +0800

    update

--- pages/my/my.js
+++ pages/my/my.js
@@ -29,6 +29,17 @@ Page({
     })
     // 获取新闻收藏列表
     this.getMyFavorites()
+    // 提示框
+    //title：提示内容
+    // duration：提示延迟时间，若时间太长可用wx.hideToast()关闭提示框
+    // icon: 图标，只有三种选择——success、loading、None，success"、"loading"会使得title最多显示七个字，"None"则使得title最多显示两行
+    // image：自定义本地图标路径，优先级比icon高
+    // mask：是否显示透明的遮罩层，防止触摸穿透，默认值为false
+    wx.showToast({
+      title: '登录成功',
+      duration: 1000,
+      icon: "success"
+    })
   },
 
   // 更新收藏
